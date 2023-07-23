eb6bebfe107e040622bf08e27b85d18becf25e26
amgd-thabit <amgadthabit9@gmail.com>
2023-07-23 02:42:23 -0700

    check

--- amgad.c
+++ amgad.c
@@ -3,78 +3,76 @@
 
 int _printf(const char *format, ...)
 {
-    int counter = 0, num, num_char;
-    char current_char, c, *str, buffer[12];
+	int counter = 0, num, num_char;
+	char current_char, c, *str, buffer[12];
 
-    va_list list;
-    va_start(list, format);
+	va_list list;
 
-    while ((current_char = *format) != '\0')
-    {
-        if (current_char == '%')
-        {
-            format++;
-            current_char = *format;
+	va_start(list, format);
 
-            if (current_char == '\0')
-            {
-                putchar('%');
-                counter++;
-                break;
-            }
-
-            if (current_char == 'c')
-            {
-                c = va_arg(list, int);
-                putchar(c);
-                counter++;
-            }
-            else if (current_char == 's')
-            {
-                str = va_arg(list, char *);
-                while (*str != '\0')
-                {
-                    putchar(*str);
-                    str++;
-                    counter++;
-                }
-            }
-            else if (current_char == 'd' || current_char == 'i')
-            {
-                num = va_arg(list, int);
-                num_char = sprintf(buffer, "%d", num);
-                printf("%s", buffer);
-                counter += num_char;
-            }
-            else if (*format == 'b') {
-                unsigned int bun = va_arg(list, unsigned int);
-                int num_bits = sizeof(unsigned int) + 3;
-                for (int i = num_bits - 1; i >= 0; i--) {
-                    int bit = (bun >> i) & 1;
-                    putchar('0' + bit);
-                    counter++;
-                }
-            }
-            else if (current_char == '%')
-            {
-                putchar('%');
-                counter++;
-            }
-            else
-            {
-                putchar('%');
-                putchar(current_char);
-                counter += 2;
-            }
-        }
-        else
-        {
-            putchar(current_char);
-            counter++;
-        }
-        format++;
-    }
-
-    va_end(list);
-    return counter;
+	while ((current_char = *format) != '\0')
+	{
+		if (current_char == '%')
+		{
+			format++;
+			current_char = *format;
+			if (current_char == '\0')
+			{
+				putchar('%');
+				counter++;
+				break;
+			}
+			if (current_char == 'c')
+			{
+				c = va_arg(list, int);
+				putchar(c);
+				counter++;
+			}
+			else if (current_char == 's')
+			{
+				str = va_arg(list, char *);
+				while (*str != '\0')
+				{
+					putchar(*str);
+					str++;
+					counter++;
+				}
+			}
+			else if (current_char == 'd' || current_char == 'i')
+			{
+				num = va_arg(list, int);
+				num_char = sprintf(buffer, "%d", num);
+				printf("%s", buffer);
+				counter += num_char;
+			}
+			else if (*format == 'b') {
+				unsigned int bun = va_arg(list, unsigned int);
+				int num_bits = sizeof(unsigned int) + 3;
+				for (int i = num_bits - 1; i >= 0; i--) {
+					int bit = (bun >> i) & 1;
+					putchar('0' + bit);
+					counter++;
+				}
+			}
+			else if (current_char == '%')
+			{
+				putchar('%');
+				counter++;
+			}
+			else
+			{
+				putchar('%');
+				putchar(current_char);
+				counter += 2;
+			}
+		}
+		else
+		{
+			putchar(current_char);
+			counter++;
+		}
+		format++;
+	}
+	va_end(list);
+	return counter;
 }
